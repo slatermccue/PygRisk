d4bc4cd02b3fe3edeb2ffec520c380789179c86d
slatermccue <mike.mccue@stratford.org>
2024-06-10 22:14:37 -0400

    switched to VSCode; added list

--- testMaskClickerV3.py
+++ testMaskClickerV3.py
@@ -3,20 +3,23 @@ import pygame as pg
 screen = pg.display.set_mode((800, 600))
 
 class Blob:
-    def __init__(self, centerPoint):
+    def __init__(self, centerPoint, bgColor):
         self.image = pg.image.load('blob.png').convert_alpha()
         self.image = pg.transform.scale(self.image, (300, 200))
         self.rect = self.image.get_rect(center=centerPoint)
         self.mask = pg.mask.from_surface(self.image)
+        self.bgColor = bgColor
 
 running = True
-
-blob = Blob((100,300))
-blob2 = Blob((400,300))
 
 white = (255,255,255)
 blue = (0,0,255)
 red = (255,0,0)
+
+blob = Blob((100,300), blue)
+blob2 = Blob((400,300), red)
+
+allBlobs = [blob, blob2]
 
 while running:
     screen.fill(white)
@@ -25,15 +28,16 @@ while running:
             running = False
 
     pos = pg.mouse.get_pos()
-    pos_in_mask = pos[0] - blob.rect.x, pos[1] - blob.rect.y
-    pos2_in_mask = pos[0] - blob2.rect.x, pos[1] - blob2.rect.y
-    touching = blob.rect.collidepoint(*pos) and blob.mask.get_at(pos_in_mask)
-    touching2 = blob2.rect.collidepoint(*pos) and blob2.mask.get_at(pos2_in_mask)
+    for entity in allBlobs:
+        pos_in_mask = pos[0] - entity.rect.x, pos[1] - entity.rect.y
+        #pos2_in_mask = pos[0] - blob2.rect.x, pos[1] - blob2.rect.y
+        touching = entity.rect.collidepoint(*pos) and entity.mask.get_at(pos_in_mask)
+        #touching2 = blob2.rect.collidepoint(*pos) and blob2.mask.get_at(pos2_in_mask)
 
-    if (touching):
-        screen.fill(red)
-    if (touching2):
-        screen.fill(blue)
+        if (touching):
+            screen.fill(entity.bgColor)
+    #if (touching2):
+    #    screen.fill(blue)
 
     screen.blit(blob.image, blob.rect)
     screen.blit(blob2.image, blob2.rect)
