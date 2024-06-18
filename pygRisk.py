bbcd1695df597fad958cdbb3f3258b3ebc47a195
slatermccue <slater.mccue@gmail.com>
2024-06-17 23:42:23 -0400

    cleanup of program file

--- pygRisk.py
+++ pygRisk.py
@@ -2,16 +2,8 @@ import pygame as pg
 
 screen = pg.display.set_mode((800, 600))
 
-class Blob:
-    def __init__(self, centerPoint, bgColor):
-        self.image = pg.image.load('blob.png').convert_alpha()
-        self.image = pg.transform.scale(self.image, (300, 200))
-        self.rect = self.image.get_rect(center=centerPoint)
-        self.mask = pg.mask.from_surface(self.image)
-        self.bgColor = bgColor
-
 class Country:
-    def __init__(self, imageName):
+    def __init__(self, imageName, name):
         self.imageNormal = pg.image.load('country'+imageName+'.png')
         #self.imageAttack = pg.image.load('attackingCountry'+imageName+'.png')
         #self.imageDefend = pg.image.load('defendingCountry'+imageName+'.png')
@@ -20,6 +12,7 @@ class Country:
         self.mask = pg.mask.from_surface(self.imageNormal)
         self.status = "normal" # hover; attacking; defending
         self.image = self.imageNormal
+        self.name = name
 
     def setStatus(self, status):
         self.status = status
@@ -47,24 +40,17 @@ white = (255,255,255)
 blue = (0,0,255)
 red = (255,0,0)
 
-#blob = Blob((100,300), blue)
-#blob2 = Blob((400,300), red)
-
-#allBlobs = [blob, blob2]
-mikem = Country('MikemCcu')
-hollyh = Country('HolLyh')
-jenniferg = Country('Jenniferg')
-paulce = Country('Paulce')
+mikem = Country('MikemCcu', 'mikemccu')
+hollyh = Country('HolLyh', 'hollyh')
+jenniferg = Country('Jenniferg', 'jenniferg')
+paulce = Country('Paulce', 'paulce')
 allCountries = [mikem,hollyh,jenniferg,paulce]
 background = pg.image.load("mapBackground.png")
-#countryMikemCcu = pg.image.load("countryMikemCcu.png")
+
 countryMikemIl = pg.image.load("countryMikemIl.png")
 countryPame = pg.image.load("countryPame.png")
-#countryPaulce = pg.image.load("countryPaulce.png")
 countryTreNeeu = pg.image.load("countryTreNeeu.png")
 countryHolLye = pg.image.load("countryHolLye.png")
-#countryHolLyh = pg.image.load("countryHolLyh.png")
-#countryJenniFerg = pg.image.load("countryJenniFerg.png")
 countryJesUsri = pg.image.load("countryJesUsri.png")
 countryLap = pg.image.load("countryLap.png")
 
@@ -73,41 +59,29 @@ while running:
     screen.blit(background, (0,0))
     for country in allCountries:
         country.draw()
-    #screen.blit(countryMikemCcu, (0,0))
     screen.blit(countryHolLye, (0,0))
-    #screen.blit(countryHolLyh, (0,0))
-    #screen.blit(countryJenniFerg, (0,0))
     screen.blit(countryJesUsri, (0,0))
     screen.blit(countryLap, (0,0))
     screen.blit(countryMikemIl, (0,0))
     screen.blit(countryPame, (0,0))
-    #screen.blit(countryPaulce, (0,0))
     screen.blit(countryTreNeeu, (0,0))
-
-
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
 
     pos = pg.mouse.get_pos()
-   # for entity in allBlobs:
-   #     pos_in_mask = pos[0] - entity.rect.x, pos[1] - entity.rect.y
-   #     touching = entity.rect.collidepoint(*pos) and entity.mask.get_at(pos_in_mask)
 
     for country in allCountries:
         pos_in_mask = pos[0] - country.rect.x, pos[1] - country.rect.y
         touching = country.rect.collidepoint(*pos) and country.mask.get_at(pos_in_mask)
     
         if (touching):
-            #screen.fill(entity.bgColor)
             country.setStatus('hover')
         else:
             country.setStatus('normal')
     
 
-    #screen.blit(blob.image, blob.rect)
-    #screen.blit(blob2.image, blob2.rect)
     #card testing below...
     #screen.blit(leftSideIndex, (50,50))
     #screen.blit(rightSideIndex, (160,50))
